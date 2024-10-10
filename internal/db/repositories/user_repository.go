97ed93cc9266c68f7015f96981c56d80c713a69d
Riki Itokazu <itokazuriki@gmail.com>
2024-10-10 14:08:01 -0400

    fix: registration dao

--- internal/db/repositories/user_repository.go
+++ internal/db/repositories/user_repository.go
@@ -3,7 +3,6 @@ package repositories
 import (
 	"context"
 	"errors"
-	"log"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -65,45 +64,35 @@ func (ur *UserRepository) Login(user *models.User) error {
 
 func (ur *UserRepository) Register(user *models.User) error {
 	pool := ur.db
-	if pool == nil {
-		log.Fatal("FAILURE")
-		var maps map[string]string
-		return maps
-	}
-	userSuccess := make(map[string]string)
+
 	// Determine if email already exists
 	var emailExists bool
 	err := pool.QueryRow(context.Background(), `SELECT EXISTS (
 			SELECT 1
 			FROM users
 			WHERE email = $1
-		)`, req.Email).Scan(&emailExists)
+		)`, user.Email).Scan(&emailExists)
 	if err != nil {
-		//http.Error(w, err.Error(), http.StatusBadRequest)
-		userSuccess["successStatus"] = err.Error()
-		return userSuccess
+		return err
 	}
 	if emailExists {
-		userSuccess["successStatus"] = "Email already exists"
-		return userSuccess
+		return errors.New("email already exists")
 	}
 
 	// use bcrypt to encrypt password
-	passwordHash, err := bcrypt.GenerateFromPassword([]byte(req.Password), 10)
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
 	if err != nil {
-		userSuccess["successStatus"] = err.Error()
-		return userSuccess
+		return err
 	}
 
 	insertQuery := `
 		INSERT INTO users (name, email, password, registered_courses, date_created, last_active)
 		VALUES ($1, $2, $3, $4, $5, $6)
 		`
-	_, err = pool.Exec(context.Background(), insertQuery, req.Name, req.Email, string(passwordHash), req.RegisteredCourses, time.Now(), time.Now())
+	_, err = pool.Exec(context.Background(), insertQuery,
+		user.Name, user.Email, string(passwordHash), user.RegisteredCourses, time.Now(), time.Now())
 	if err != nil {
-		userSuccess["successStatus"] = "Error occured while inserting data"
-		return userSuccess
+		return err
 	}
-	userSuccess["successStatus"] = "true"
-	return userSuccess
+	return nil
 }

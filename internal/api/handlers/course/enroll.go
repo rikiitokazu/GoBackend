9cfdc4be9cc5268ab896a4004074ac778d6ffcf7
Riki Itokazu <itokazuriki@gmail.com>
2024-10-25 21:29:21 -0400

    chore: jwt fix

--- internal/api/handlers/course/enroll.go
+++ internal/api/handlers/course/enroll.go
@@ -2,9 +2,12 @@ package course
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/rikiitokazu/go-backend/internal/api/models"
 )
 
@@ -14,9 +17,24 @@ func (ch *CourseHandler) EnrollCourse(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	// Valid jwt
+	tokenString := r.Header.Get("Authorization")
+	if tokenString == "" {
+		w.WriteHeader(http.StatusUnauthorized)
+		log.Println("No jwt")
+		return
+	}
+	tokenString = tokenString[len("Bearer: "):]
 
+	err := verifyToken(tokenString)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		log.Println("Invalid jwt")
+		return
+	}
+	log.Println("Valid jwt")
 	// Check availability of course in "courses" table
-	err := ch.CourseRepository.EnrollCourse(&req)
+	err = ch.CourseRepository.EnrollCourse(&req)
 	if err != nil {
 		log.Println("error")
 	}
@@ -25,4 +43,21 @@ func (ch *CourseHandler) EnrollCourse(w http.ResponseWriter, r *http.Request) {
 	// Add to database
 
 	// Return http response
+}
+
+// TODO: move to utils
+func verifyToken(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("TOKEN_SECRET")), nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if !token.Valid {
+		return errors.New("invalid jwt")
+	}
+
+	return nil
 }
